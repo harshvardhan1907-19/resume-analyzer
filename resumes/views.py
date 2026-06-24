d43ba8b660b8e7550d0a6528e34f5d29b71a1121
harshvardhan1907-19 <sagarharshvardhan9@gmail.com>
2026-06-24 22:43:30 +0530

    Major changes

--- resumes/views.py
+++ resumes/views.py
@@ -124,8 +124,8 @@ class ResumeAnalyzeView(APIView):
 
             models_to_try = [
                 'gemini-3.5-flash',
-                'gemini-2.5-flash',
-                'gemini-1.5-flash',
+                # 'gemini-2.5-flash',
+                # 'gemini-1.5-flash',
             ]
             
             response = None
